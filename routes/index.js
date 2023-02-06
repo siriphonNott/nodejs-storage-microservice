493160b72d63745a2707027c6e46bda97c0805d4
nottdark <nottdark.dev@gmail.com>
2023-02-07 01:37:16 +0700

    Add route /api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
-import { Router } from 'express'
-import api from './api/index.js'
-import staticRoute from './api/static.js'
+import { Router } from "express";
+import api from "./api/index.js";
+import staticRoute from "./api/static.js";
+import config from "../configs/app.js";
 const router = Router();
 
 router.use(`/api/v1`, api);
 router.use(`/static`, staticRoute);
+router.use("/api$/", (req, res) => {
+  res.send({
+    apiVersion: +config.apiVersion,
+    buildVersion: +config.buildVersion,
+  });
+});
 
-export default router
+export default router;
